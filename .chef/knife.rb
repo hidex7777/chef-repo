c15ec74a94de84f492fc183d5fdfea1f1d148b47
Hideo Saito <hidex7777@gmail.com>
2014-05-12 00:57:31 +0900

    berkshelf

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,4 +1,4 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
+cookbook_path    ["cookbooks", "site-cookbooks", "berks-cookbooks"]
 node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
---
+++ Berksfile
@@ -0,0 +1,4 @@
+source "https://api.berkshelf.com"
+
+cookbook 'ruby_build'
+cookbook 'rbenv', github: "fnichol/chef-rbenv"
